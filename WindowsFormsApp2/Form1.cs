c79a16afb3bfc539c2005cbbd2c89d3885e1c949
Александра Никитина <aglerika@outlook.com>
2020-07-02 15:36:18 +0300

    next generation

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -14,8 +14,10 @@ namespace WindowsFormsApp2
     {
 
         private int resolution;
-
+        private bool[,] field;
         private Graphics graphics;
+        private int rows;
+        private int cols;
 
         public Form1()
         {
@@ -34,6 +36,46 @@ namespace WindowsFormsApp2
             resolution = (int)nudResolution.Value;
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             graphics = Graphics.FromImage(pictureBox1.Image);
+
+            rows = pictureBox1.Height / resolution;
+            cols = pictureBox1.Width / resolution;
+
+            field = new bool[cols, rows];
+
+            //первое поколение
+            Random rand = new Random();
+            for (int x=0; x<cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = rand.Next((int)nudDancity.Value) == 0;
+                    if (field[x, y])
+                    {
+                        //graphics.FillRectangle(Brushes.CornflowerBlue, x * resolution, y * resolution, resolution, resolution);
+                    }
+                }
+            }
+
+            timer1.Start();
+        }
+
+        private void NextGeneration()
+        {
+            graphics.Clear(Color.Black);
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    if (field[x, y])
+                    {
+                        //отрисовка живой клетки
+                        graphics.FillRectangle(Brushes.CornflowerBlue, x * resolution, y * resolution, resolution, resolution);
+                       
+                    }
+                }
+            }
+
         }
 
         private void StopGame()
@@ -52,14 +94,16 @@ namespace WindowsFormsApp2
             StartGame();
         }
 
+        //Обработка нажатия кнопки Stop
         private void btnstop_Click(object sender, EventArgs e)
         {
             StopGame();
         }
 
+        //Обработка Timer
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            NextGeneration();
         }       
     }
 }
